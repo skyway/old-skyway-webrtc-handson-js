84ab8d14e549d4a46f4f7c178660f897aec0c370
Alan Iida <alan.m.shelly@gmail.com>
2015-07-08 17:14:47 +0900

    add step 4

--- script.js
+++ script.js
@@ -53,15 +53,31 @@ $(document).ready(function () {
 
 
     // コールの追加
-
+    function addCall(call){
+        callList.push(call);
+    }
 
     // コールの削除
-
+    function removeCall(call){
+        var position = callList.indexOf(call);
+        if(position > 0){
+            callList.splice(position,1)
+        }
+    }
 
     // VIDEO要素を追加する
+    function addVideo(call,stream){
+        var videoDom = $('<video autoplay>');
+        videoDom.attr('id',call.peer);
+        videoDom.prop('src',URL.createObjectURL(stream));
 
+        $('.videosContainer').append(videoDom);
+    }
 
     // VIDEO要素を削除する
+    function removeVideo(call){
+        $('#'+call.peer).remove();
+    }
 
 
 });
