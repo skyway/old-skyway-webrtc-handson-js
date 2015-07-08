83c94abca7dd48058991a8db3d4ca6b7b2d0a0f1
Alan Iida <alan.m.shelly@gmail.com>
2015-07-08 17:19:17 +0900

    add step 5

--- script.js
+++ script.js
@@ -43,10 +43,34 @@ $(document).ready(function () {
     });
 
     // callイベント用のハンドラを設置
+    peer.on('call', function(call) {
+        // 相手からcallイベントがきたらstreamを送り返す（応答する）
+        call.answer(myStream);
 
+        // callオブジェクトのイベントをセット
+        setupCallEventHandlers(call);
+
+        // 接続先毎のcallオブジェクトをマルチパーティ管理用に保存
+        addCall(call);
+
+    });
 
     // callオブジェクトのイベントをセットする
+    function setupCallEventHandlers(call){
+        // 相手からstreamイベンがきたらそのstreamをVIDEO要素に表示する
+        call.on('stream', function(stream){
+            addVideo(call,stream);
+        });
 
+        // 相手からcloseイベントがきたらコネクションを切断して保存した
+        // callオブジェクトを削除、対応するVIDEOS要素も削除
+        call.on('close', function(){
+            call.close();
+            removeCall(call);
+            removeVideo(call);
+        });
+
+    }
 
     // ユーザリストを取得して片っ端から繋ぐ
 
