f6decec3966568ab2e7e6691358c3e575161ac8a
Alan Iida <alan.m.shelly@gmail.com>
2015-07-08 17:01:53 +0900

    add step 2

--- script.js
+++ script.js
@@ -19,10 +19,18 @@ $(document).ready(function () {
       navigator.mozGetUserMedia;
 
     // Peerオブジェクトを生成
+    peer = new Peer({key: APIKEY, debug: 3});
 
     // エラーハンドラ
+    peer.on('error', function(err){
+        console.error(err);
+    });
 
     // openイベントのハンドラ
+    peer.on('open', function(id) {
+        myPeerid = id;
+        console.log('My peer ID is: ' + id);
+    });
 
     // callイベント用のハンドラを設置
 
